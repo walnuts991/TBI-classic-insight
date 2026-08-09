13b1edd027dec2a375d99ec66140989d2875d626
gauri <ijsgauri@gmail.com>
2026-08-09 20:51:33 +0530

    Fix chat routes

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const { chatWithAI } = require("../controllers/chatController");
-const { protect } = require("../middleware/authMiddleware");
+const { protect } = require("../Middleware/authMiddleware");
 
 router.post("/", protect, chatWithAI);
 
